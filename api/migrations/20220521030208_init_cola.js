3cce26eeddd39e345ec89f19affdafb1d7c9c47f
Richardaeld <Richardaeld@gmail.com>
2022-05-21 16:45:39 -0400

    Syntax corrections

--- api/migrations/20220521030208_init_cola.js
+++ api/migrations/20220521030208_init_cola.js
@@ -6,15 +6,15 @@ exports.up = function(knex) {
     return knex.schema.createTable('cola', tbl => {
         tbl.increments();
         tbl.string('name', 50)
-            .notNullable;
-        tbl.float('price')
-            .notNullable;
+            .notNullable();
+        tbl.float('price', 8, 2)
+            .notNullable();
         tbl.integer('amount')
-            .notNullable;
+            .notNullable();
         tbl.integer('max_amount')
-            .notNullable;
-        tbl.string('description')
-            .notNullable;
+            .notNullable();
+        tbl.text('description')
+            .notNullable();
   })
 };
 
