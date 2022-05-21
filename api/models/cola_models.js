9b01443d5b2f640441711293b7782846f79735c2
Richardaeld <Richardaeld@gmail.com>
2022-05-20 23:31:54 -0400

    Added models file with comments of antipicated calls

---
+++ api/models/cola_models.js
@@ -0,0 +1,17 @@
+const db = require('../db_config');
+
+module.exports = {
+
+};
+
+// ---------------------Cola
+// Add Cola(s)
+// All View Cola (with description)
+// Restocker View All Cola (without description)
+// View Single Cola
+// Patch 'name' of Single Cola
+// Patch 'amount' of Single Cola
+// Patch 'max_amount' of Single Cola
+// Patch 'price' of Single Cola
+// Patch 'description' of Single Cola
+// Put (update) entire Single Cola
