3088023cb21c4cc9ef7946643e54e86c590c5cf5
Richardaeld <Richardaeld@gmail.com>
2022-05-20 21:54:04 -0400

    Added a welcome message at api root

--- api/app/server.js
+++ api/app/server.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.json({message : "Welcome to ColaCo Vending Machine's API!"})
+})
+
 module.exports = server;
