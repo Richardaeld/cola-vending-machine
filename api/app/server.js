02153eb7edc737be2a886c0032689ae0b7a82fe8
Richardaeld <Richardaeld@gmail.com>
2022-05-21 00:10:53 -0400

    Added routes to server

--- api/app/server.js
+++ api/app/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+// Routes
+const cola_routes = require('../routes/cola_routes');
+
 const server = express();
 
 server.use(express.json());
@@ -7,5 +10,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.json({message : "Welcome to ColaCo Vending Machine's API!"});
 })
+
+// Server Route Exports
+server.use('/cola', cola_routes);
 
 module.exports = server;
