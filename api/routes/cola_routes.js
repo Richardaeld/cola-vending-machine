b9116d39b6fe930a762fab828d874e6e75cbda08
Richardaeld <Richardaeld@gmail.com>
2022-05-20 23:54:48 -0400

    Created two routes for testing

--- api/routes/cola_routes.js
+++ api/routes/cola_routes.js
@@ -4,13 +4,43 @@ const db = require('../models/cola_models');
 const router = expresss.Router();
 
 // ---------------------Cola
-// Add Cola(s)
-// All View Cola (with description)
-// Restocker View All Cola (without description)
-// View Single Cola
+// Post Cola(s)
+router.post('/add', (req, res) => {
+    db.addCola(req.body)
+        .then((cola) => {
+            if (cola) {
+                res.status(200).json({
+                    message: `${cola.length} cola(s) have been added and they are:`,
+                    cola
+                });
+            } else {
+                res.status(404).json({ message: 'Record was not inserted' });
+            }
+        })
+        .then((error) => {
+            res.status(500).json({ message: `An error has occured: ${error}` });
+        });
+});
+
+
+// Get All Cola (with description)
+router.get('/getAll', (req, res) => {
+    db.viewAllCola()
+        .then((cola) => {
+            res.status(200).json({ cola });
+        })
+        .then((error) => {
+            res.status(500).json({ message: `An error has occured: ${error}` });
+        });
+});
+
+// Restocker Get All Cola (without description)
+// Get Single Cola
 // Patch 'name' of Single Cola
 // Patch 'amount' of Single Cola
 // Patch 'max_amount' of Single Cola
 // Patch 'price' of Single Cola
 // Patch 'description' of Single Cola
 // Put (update) entire Single Cola
+
+module.exports = router;
