cd616163ced113ee6bd00793150e65480c6577c8
Richardaeld <Richardaeld@gmail.com>
2022-05-20 22:43:10 -0400

    Added additiona configs for sqlite db

--- api/knexfile.js
+++ api/knexfile.js
@@ -6,21 +6,27 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './dev.sqlite3'
-    }
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
   },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     rejectUnauthorized: false,
     migrations: {
       tableName: 'knex_migrations',
       directory: './migrations',
-    }
-  }
+    },
+  },
 };
