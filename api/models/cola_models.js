9cc8fa25883e42d6db2752958f41c99b92b93f96
Richardaeld <Richardaeld@gmail.com>
2022-05-20 23:35:06 -0400

    Added two models for testing

--- api/models/cola_models.js
+++ api/models/cola_models.js
@@ -6,7 +6,15 @@ module.exports = {
 
 // ---------------------Cola
 // Add Cola(s)
+async function addCola (cola) {
+    return await db('cola').insert(cola, ['id', 'name', 'amount', 'max_amount', 'description']);
+}
+
 // All View Cola (with description)
+function viewAllCola () {
+    return db('cola');
+}
+
 // Restocker View All Cola (without description)
 // View Single Cola
 // Patch 'name' of Single Cola
