234ee164e134bd5e9d46a3cc54f122ade3a86d9c
Richardaeld <Richardaeld@gmail.com>
2022-05-23 15:12:08 -0400

    Created debug for postgres

--- api/auth/auth_routes.js
+++ api/auth/auth_routes.js
@@ -66,7 +66,7 @@ router.post('/login', (req, res) => {
             }
         })
         .catch((error) => {
-            res.status(500).json({ message: `An error occured!` });
+            res.status(500).json({ message: `An error occured! ${error}` });
         });
 });
 
