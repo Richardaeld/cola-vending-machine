9c461ff3398370109846dad99bdbda407cdac889
Richardaeld <Richardaeld@gmail.com>
2022-05-24 19:42:27 -0400

    Created temp main

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -10,10 +10,23 @@ function App() {
       <header class="container-fluid">
         <div class="row justify-content-center">
           <div class="col-12 text-center">
-            <h1 class="top-100 position-relative">ColaCo Vending</h1>
+            <h1 class="mt-5 pt-5">ColaCo Vending</h1>
           </div>
         </div>
       </header>
+
+      <main class="container-fluid">
+        <div class="row justify-content-between">
+          <section class="col-9">
+            <h2>Buy a cola</h2>
+
+          </section>
+          <section class="col-3">
+            <h2>Pay for cola</h2>
+
+          </section>
+        </div>
+      </main>
     </>
 
 
