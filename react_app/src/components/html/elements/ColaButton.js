9350b7d2f55f43ab3537e330962c1ac47c6edb10
Richardaeld <Richardaeld@gmail.com>
2022-05-25 12:46:45 -0400

    abstracted cola template to its own component

---
+++ react_app/src/components/html/elements/ColaButton.js
@@ -0,0 +1,28 @@
+// Cola button for vending machine
+export default function ColaButton (props) {
+
+
+    return (
+        <div class="col-3">
+        {/* header */}
+        <h3>Fizz</h3>
+        {/* body */}
+        <div>
+          <div>
+            <span>Price </span>
+            <span>$1.00</span>
+          </div>
+          <div>
+            <span>Amount Available </span>
+            <span>100</span>
+          </div>
+          <div>
+            <span>Description </span>
+            <span>An effervescent fruity experience with hints of grape and coriander.</span>
+          </div>
+        </div>
+        {/* buy button */}
+        <button>Buy now</button>
+      </div>
+    )
+}
