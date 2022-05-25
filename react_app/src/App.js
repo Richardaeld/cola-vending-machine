f72e22612bc133646b581cdaaba9f1d09e992890
Richardaeld <Richardaeld@gmail.com>
2022-05-25 12:32:50 -0400

    created cola template

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -19,7 +19,35 @@ function App() {
         <div class="row justify-content-between">
           <section class="col-9">
             <h2>Buy a cola</h2>
+            <div class="row justify-content-around">
+              
+              {/* Cola Template */}
+              <div class="col-3">
+                {/* header */}
+                <h3>Fizz</h3>
+                {/* body */}
+                <div>
+                  <div>
+                    <span>Price </span>
+                    <span>$1.00</span>
+                  </div>
+                  <div>
+                    <span>Amount Available </span>
+                    <span>100</span>
+                  </div>
+                  <div>
+                    <span>Description </span>
+                    <span>An effervescent fruity experience with hints of grape and coriander.</span>
+                  </div>
+                </div>
+                {/* buy button */}
+                <button>Buy now</button>
+              </div>
 
+
+
+
+            </div>
           </section>
           <section class="col-3">
             <h2>Pay for cola</h2>
