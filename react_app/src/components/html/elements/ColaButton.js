a03b717e6be3328befabe1b8d683bd5ed1ceffb1
Richardaeld <Richardaeld@gmail.com>
2022-05-25 14:32:57 -0400

    Added dynamic content

--- react_app/src/components/html/elements/ColaButton.js
+++ react_app/src/components/html/elements/ColaButton.js
@@ -1,26 +1,27 @@
 // Cola button for vending machine
 export default function ColaButton (props) {
+
     return (
         <div class="col-3">
-        {/* header */}
-        <h3>Fizz</h3>
-        {/* body */}
-        <div>
-          <div>
-            <span>Price </span>
-            <span>$1.00</span>
-          </div>
-          <div>
-            <span>Amount Available </span>
-            <span>100</span>
-          </div>
-          <div>
-            <span>Description </span>
-            <span>An effervescent fruity experience with hints of grape and coriander.</span>
-          </div>
-        </div>
-        {/* buy button */}
-        <button>Buy now</button>
+            {/* Header */}
+            <h3>{props.name}</h3>
+            {/* body */}
+            <div>
+                <div>
+                    <span>Price: </span>
+                    <span>${ props.price.toFixed(2)}</span>
+                </div>
+                <div>
+                    <span>Amount Available: </span>
+                    <span>{props.amount}</span>
+                </div>
+                <div>
+                    <span>Description: </span>
+                    <span>{props.details}</span>
+                </div>
+            </div>
+            {/* Purchase Button */}
+            <button>Buy now</button>
       </div>
     )
 }
