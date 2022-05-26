ccd707343d0cbb4317596c89a1cb3708281eff24
Richardaeld <Richardaeld@gmail.com>
2022-05-26 19:45:43 -0400

    Moved admin restricted routes to new route

---
+++ api/routes/user_admin_routes.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const dbUser = require('../models/user_models');
+const dbCola = require('../models/cola_models');
+
+const router = express.Router();
+
+// ---------------------User Admin Auth
+router.get('/getAll/user', (req, res) => {
+    dbUser.findAllUsers()
+        .then((users) => {
+            res.status(200).json({ users });
+        })
+        .catch((error) => {
+            res.status(500).json({ message: `An error has occured: ${error}` })
+        });
+});
+
+router.get('/getAll/admin', (req, res) => {
+    dbUser.findAllAdmin()
+        .then((users) => {
+            res.status(200).json({ users });
+        })
+        .catch((error) => {
+            res.status(500).json({ message: `An error has occured: ${error}` })
+        });
+});
+
+router.get('/getAll/adminAndUser', (req, res) => {
+    dbUser.findAllAdminAndUsers()
+        .then((users) => {
+            res.status(200).json({ users });
+        })
+        .catch((error) => {
+            res.status(500).json({ message: `An error has occured: ${error}` })
+        });
+});
+
+// ---------------------Auth
+// Change user is_admin status
+router.patch('/patch/is_admin/:id', (req, res) => {
+    const { id } = req.params;
+    const { change } = req.body;
+
+    if (!(change === false || change === true)) {
+        return res.status(400).json({ message: 'Please true or false' });
+    }
+
+    db.updateUserName(id, change)
+        .then((user) => {
+            if(user) {
+                res.status(200).json({
+                    message: 'The updated user:',
+                    user
+                });
+            } else {
+                res.status(404).json({ message: 'Record not found' });
+            }
+        })
+        .catch((error) => {
+            res.status(404).json({ message: `An error occured: ${error}` });
+        });
+});
+
+// Delete User
+router.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+
+    dbUser.deleteUser(id)
+        .then((user) => {
+            if (user > 0) {
+                res.status(200).json({ message: `User with id ${id} has been removed` });
+            } else {
+                res.status(404).json({ message: 'No user with that id exists' });
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({ message: `An error occured: ${error}` });
+        });
+});
+
+
+module.exports = router;
--- api/routes/user_auth_routes.js
+++ api/routes/user_auth_routes.js
@@ -4,63 +4,6 @@ const dbCola = require('../models/cola_models');
 
 const router = express.Router();
 
-// ---------------------User Admin Auth
-router.get('/getAll/user', (req, res) => {
-    dbUser.findAllUsers()
-        .then((users) => {
-            res.status(200).json({ users });
-        })
-        .catch((error) => {
-            res.status(500).json({ message: `An error has occured: ${error}` })
-        });
-});
-
-router.get('/getAll/admin', (req, res) => {
-    dbUser.findAllAdmin()
-        .then((users) => {
-            res.status(200).json({ users });
-        })
-        .catch((error) => {
-            res.status(500).json({ message: `An error has occured: ${error}` })
-        });
-});
-
-router.get('/getAll/adminAndUser', (req, res) => {
-    dbUser.findAllAdminAndUsers()
-        .then((users) => {
-            res.status(200).json({ users });
-        })
-        .catch((error) => {
-            res.status(500).json({ message: `An error has occured: ${error}` })
-        });
-});
-
-// ---------------------Auth
-// Change user is_admin status
-router.patch('/patch/is_admin/:id', (req, res) => {
-    const { id } = req.params;
-    const { change } = req.body;
-
-    if (!(change === false || change === true)) {
-        return res.status(400).json({ message: 'Please true or false' });
-    }
-
-    db.updateUserName(id, change)
-        .then((user) => {
-            if(user) {
-                res.status(200).json({
-                    message: 'The updated user:',
-                    user
-                });
-            } else {
-                res.status(404).json({ message: 'Record not found' });
-            }
-        })
-        .catch((error) => {
-            res.status(404).json({ message: `An error occured: ${error}` });
-        });
-});
-
 // Change user name
 router.patch('/patch/name/:id', (req, res) => {
     const { id } = req.params;
@@ -111,23 +54,6 @@ router.patch('/patch/password/:id', (req, res) => {
         })
         .catch((error) => {
             res.status(404).json({ message: `An error occured: ${error}` });
-        });
-});
-
-// Delete User
-router.delete('/delete/:id', (req, res) => {
-    const { id } = req.params;
-
-    dbUser.deleteUser(id)
-        .then((user) => {
-            if (user > 0) {
-                res.status(200).json({ message: `User with id ${id} has been removed` });
-            } else {
-                res.status(404).json({ message: 'No user with that id exists' });
-            }
-        })
-        .catch((error) => {
-            res.status(500).json({ message: `An error occured: ${error}` });
         });
 });
 
