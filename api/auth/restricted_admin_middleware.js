2a4f075d63973425c7a3f9251c040da9beb79a61
Richardaeld <Richardaeld@gmail.com>
2022-05-26 21:36:01 -0400

    Removed console.log

--- api/auth/restricted_admin_middleware.js
+++ api/auth/restricted_admin_middleware.js
@@ -11,7 +11,6 @@ module.exports = (req, res, next) => {
             } else {
                 req.decodedToken = decodedToken;
 
-                console.log(decodedToken.admin)
                 if (decodedToken.admin == true) {
                     next();
                 } else {
