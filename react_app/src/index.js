1a5e6859e1b8b42478f1c7fdc83c080a92d04131
Richardaeld <Richardaeld@gmail.com>
2022-06-10 19:58:34 -0400

    Abstracted css into multiple files

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './components/css/art/VendingMachine.css';
+import './components/css/art/Background.css';
+import './components/css/animation/NeonFlicker.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Elements} from '@stripe/react-stripe-js'
